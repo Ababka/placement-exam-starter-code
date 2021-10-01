e33c888e3c6721b160bc27861ff40b8e2d286e69
Anthony Babka <anthonybabka@gmail.com>
2021-09-30 21:07:05 -0700

    :x
    my assessment thank you

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,61 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
-// We'll use these variables to track the counts of each cookie type
+let yourName = "Anthony Babka"
 let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
+let cookieTotal = 0
 
-// Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
+//document.getElementById('#qty-total').textContent = cookieTotal
 
-// Event listener for clicks on the "+" button for Ginger Bread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
-
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
+let gbQty = document.querySelector("#qty-gb")
+//let gbCookie = localStorage.getItem('gbCookie')
+/*if ('gbCookie'){
+    gbQty.textContent = gbCookie;
+}*/
+document.querySelector('#add-gb').addEventListener('click', function() {
+    //let gb = parseInt(gbQty.textContent);
+    gb = gb +1;
+    gbQty.textContent = gb;
+    //localStorage.setItem('gbCookie', gb)
+    cookieTotal.textContent = gb + cc + sugar;
+    console.log("gb was pressed")
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+document.querySelector('#minus-gb').addEventListener('click', function() {
+    //let gb = parseInt(gbQty.textContent);
+    gb = gb -1;
+    gbQty.textContent = gb;
+    //localStorage.setItem('gbCookie', gb)
+    cookieTotal.textContent = gb + cc + sugar;
+    console.log("gb minus was pressed")
+})
+
+let ccQty = document.querySelector("#qty-cc")
+document.querySelector('#add-cc').addEventListener('click', function() {
+    cc = cc +1;
+    ccQty.textContent = cc;
+    cookieTotal.textContent = gb + cc + sugar;
+    console.log("cc was pressed")
+})
+
+document.querySelector('#minus-cc').addEventListener('click', function() {
+    cc = cc -1;
+    ccQty.textContent = cc;
+    cookieTotal.textContent = gb + cc + sugar;
+    console.log("cc minus was pressed")
+})
+
+let sugarQty = document.querySelector("#qty-sugar")
+document.querySelector('#add-sugar').addEventListener('click', function() {
+    sugar = sugar +1;
+    sugarQty.textContent = sugar;
+    cookieTotal.textContent = gb + cc + sugar;
+    console.log("sugar was pressed")
+})
+
+document.querySelector('#minus-sugar').addEventListener('click', function() {
+    sugar = sugar -1;
+    sugarQty.textContent = sugar;
+    cookieTotal.textContent = gb + cc + sugar;
+    console.log("sugar minus was pressed")
+}) 
